cfa916d49bf5f52a54da32e9c6c6c4c5d517291c
[Brody Van Eperen] <bvaneperen@hbs.net>
2024-06-11 14:17:13 -0500

    Add project files.

---
+++ Controllers/SpotifyController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpotifyAPI.Web;
+
+namespace SpotifyApp.Controllers
+{
+    public class SpotifyController : Controller
+    {
+
+        [Route("login")]
+        public async Task<IActionResult> Login()
+        {
+            var (verifier, challenge) = PKCEUtil.GenerateCodes(); 
+            TempData["verifier"] = verifier;    
+
+            var loginRequest = new LoginRequest(
+              new Uri("https://localhost:7118/callback"),
+              "a97145a9397d4a96b7e681552779c5fb",
+              LoginRequest.ResponseType.Code
+            )
+            {
+                CodeChallengeMethod = "S256",
+                CodeChallenge = challenge,
+                Scope = new[] { Scopes.PlaylistReadPrivate, Scopes.PlaylistReadCollaborative }
+            };
+            var uri = loginRequest.ToUri();
+
+            return Redirect(uri.ToString());
+        }
+
+        [Route("callback")]
+        public async Task<IActionResult> Callback(string code)
+        {
+            var codeVerifier = TempData["verifier"] as string;
+
+            var tokenRequest = new PKCETokenRequest(
+                "a97145a9397d4a96b7e681552779c5fb",
+                code,
+                new Uri("https://localhost:7118/callback"),
+                codeVerifier
+            );
+
+            var tokenResponse = await new OAuthClient().RequestToken(tokenRequest);
+
+            var spotify = new SpotifyClient(tokenResponse.AccessToken);
+
+            var currentUser = await spotify.UserProfile.Current();
+            ViewBag.UserName = currentUser.DisplayName;
+
+            return View("Profile"); 
+        }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿const clientId = "a97145a9397d4a96b7e681552779c5fb";
+
+function generateCodeVerifier(length) {
+    let text = ''; 
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length)); 
+    }
+
+    return text; 
+}
+
+async function generateCodeChallenge(codeVerifier) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+
