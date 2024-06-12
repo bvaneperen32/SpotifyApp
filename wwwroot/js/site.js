f1db9d5c7a056ac6ffbbd39284e203557f464723
[Brody Van Eperen] <bvaneperen@hbs.net>
2024-06-12 16:11:06 -0500

    Top artists upgrades

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,22 +1,15 @@
-﻿const clientId = "a97145a9397d4a96b7e681552779c5fb";
+﻿function initializeCarouselWithRandomBackgrounds() {
+    var backgrounds = [
+        '/images/bg1.jpg',
+        '/images/bg2.jpg',
+        '/images/bg3.jpg',
+        '/images/bg4.jpg'
+    ];
 
-function generateCodeVerifier(length) {
-    let text = ''; 
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length)); 
-    }
-
-    return text; 
-}
-
-async function generateCodeChallenge(codeVerifier) {
-    const data = new TextEncoder().encode(codeVerifier);
-    const digest = await window.crypto.subtle.digest('SHA-256', data);
-    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
+    $('#carouselExampleIndicators').on('slid.bs.carousel', function () {
+        var randomIndex = Math.floor(Math.random() * backgrounds.length);
+        var newBackground = 'url(' + backgrounds[randomIndex] + ')';
+        $('.top-artists-page').css('background-image', newBackground);
+    });
 }
 
